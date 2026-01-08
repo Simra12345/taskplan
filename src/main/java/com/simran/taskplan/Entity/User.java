1fae0a1bbdcac8121871b9c227f474d10cba1ce9
Simran Bhadana <simranbhadana7148@gmail.com>
2026-01-08 19:37:16 +0530

    user

--- src/main/java/com/simran/taskplan/Entity/User.java
+++ src/main/java/com/simran/taskplan/Entity/User.java
@@ -7,11 +7,37 @@ import jakarta.persistence.Id;
 
 @Entity
 public class User {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private long id;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
     private String Username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String password;
 
 
