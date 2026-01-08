109f7ddc2880ccbf5190979364530d21534d83c8
Simran Bhadana <simranbhadana7148@gmail.com>
2026-01-08 19:11:55 +0530

    Initial Spring Boot project structure

---
+++ src/main/java/com/simran/taskplan/TaskplanApplication.java
@@ -0,0 +1,13 @@
+package com.simran.taskplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskplanApplication.class, args);
+	}
+
+}
